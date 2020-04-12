d4e82105759310abadc05b095f330a9a31268e84
trafilaw <gitlawsserver@outlook.de>
2020-04-12 22:44:06 +0200

    Add aBuf[128]

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -769,9 +769,11 @@ void CGameContext::OnTick()
 			else if(m_VoteEnforce == VOTE_ENFORCE_NO || (m_VoteUpdate && No >= (Total+1)/2) || time_get() > m_VoteCloseTime)
 				EndVote(VOTE_END_FAIL, m_VoteEnforce==VOTE_ENFORCE_NO);
 				if(VetoStop || (m_VoteWillPass && Veto)){
+					char aBuf[128];
 					str_format(aBuf, sizeof(aBuf), "Vote failed because of veto. Find an empty server instead");
 					pSelf->SendChat(-1, CHAT_ALL, -1, aBuf);
 				} else {
+					char aBuf[128];
 					str_format(aBuf, sizeof(aBuf), "Vote failed");
 					pSelf->SendChat(-1, CHAT_ALL, -1, aBuf);
 				}
