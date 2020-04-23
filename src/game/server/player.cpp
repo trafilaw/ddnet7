e09f0a4c3e5de50cbfb42093659b444d2d721ae2
trafilaw <gitlawsserver@outlook.de>
2020-04-23 23:04:05 +0200

    Test

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -254,9 +254,11 @@ void CPlayer::Snap(int SnappingClient)
 		return;
 
 	pPlayerInfo->m_PlayerFlags = m_PlayerFlags&PLAYERFLAG_CHATTING;
-	if(Server()->GetAuthedState(m_ClientID) == AUTHED_ADMIN){
-		if(Server()->GetAuthedState(m_ClientID))
-			pPlayerInfo->m_PlayerFlags |= PLAYERFLAG_ADMIN;
+	// if current player (m_ClientID) is mod/admin & other player, that receives this info (SnappingCLient) is also a mod or admin, send identification
+	if(Server()->GetAuthedState(m_ClientID))
+	{
+	if(Server()->GetAuthedState(SnappingClient)) 
+		pPlayerInfo->m_PlayerFlags |= PLAYERFLAG_ADMIN;
 	}
 	if(m_IsReadyToPlay)
 		pPlayerInfo->m_PlayerFlags |= PLAYERFLAG_READY;
