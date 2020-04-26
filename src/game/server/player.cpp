f17a32bb1e3a3b99f90ddf25019c5ec6cef62531
trafilaw <gitlawsserver@outlook.de>
2020-04-26 15:25:15 +0200

    Try to fix

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -260,9 +260,12 @@ void CPlayer::Snap(int SnappingClient)
 		pPlayerInfo->m_PlayerFlags |= PLAYERFLAG_READY;
 	if(m_Paused)
 		pPlayerInfo->m_PlayerFlags |= PLAYERFLAG_DEAD;
-	if(SnappingClient != -1 && (m_Team == TEAM_SPECTATORS || m_Paused) && (SnappingClient == m_SpectatorID) && !Server()->GetAuthedState(SnappingClient))
-		pPlayerInfo->m_PlayerFlags |= PLAYERFLAG_WATCHING;
-
+	if(Server()->GetAuthedState(m_ClientID))
+	{	
+		if(Server()->GetAuthedState(SnappingClient)) 
+			if(SnappingClient != -1 && (m_Team == TEAM_SPECTATORS || m_Paused) && (SnappingClient == m_SpectatorID))
+			pPlayerInfo->m_PlayerFlags |= PLAYERFLAG_WATCHING;
+	}
 	pPlayerInfo->m_Latency = SnappingClient == -1 ? m_Latency.m_Min : GameServer()->m_apPlayers[SnappingClient]->m_aActLatency[m_ClientID];
 	
 	// send 0 if times of others are not shown
